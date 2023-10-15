ef50569e82a7fb73d3b983fb240fb7c26b3fcc73
Alexander Hentschel <alexander.hentschel@ea-energie.de>
2023-10-15 12:43:21 +0200

    add example

---
+++ ruff_example_github/greeting.py
@@ -0,0 +1,5 @@
+def say_hello():
+	print("hello")
+
+def say_hi():
+	print("hi")
---
+++ ruff_example_github/main.py
@@ -0,0 +1,10 @@
+import ruff_example_github.greeting as g
+
+
+def main():
+	g.say_hello()
+	g.say_hi()
+
+
+if __name__ == "__main__":
+	main()
